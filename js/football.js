0eda3c2230701f6884c3a4bf65d64f9cf80de957
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-09 23:03:44 +0000

    undid selector population refactoring as it would have slowed down code

--- js/football.js
+++ js/football.js
@@ -3,18 +3,25 @@ for (var id = 1; id <= 40; id++) {
     url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
     dataType: 'json'
     }).done(function(data) {
-        function populateSelectors(Position, selectorClass) {
-          if(data.type_name === Position) {
-           $(selectorClass).append(
-            $('<option></option>').html(data.web_name).val(data.now_cost));
-        };
-      }
-    populateSelectors("Goalkeeper", '.goalkeepers')
-    populateSelectors("Defender", '.defenders')
-    populateSelectors("Midfielder", '.midfielders')
-    populateSelectors("Forward", '.forwards')
-  });
-}
+      if(data.type_name === "Goalkeeper") {
+        $('.goalkeepers').append(
+          $('<option></option>').html(data.web_name).val(data.now_cost)
+          );
+      } else if(data.type_name === "Defender") {
+        $('.defenders').append(
+          $('<option></option>').html(data.web_name).val(data.now_cost)
+          );
+      } else if(data.type_name === "Midfielder") {
+        $('.midfielders').append(
+          $('<option></option>').html(data.web_name).val(data.now_cost)
+          );
+      } else if(data.type_name === "Forward") {
+        $('.forwards').append(
+          $('<option></option>').html(data.web_name).val(data.now_cost)
+          );
+      } else {return false}
+    });
+};
 
 function populateTeam(selectorClass, positionArray, positionClass, positionCode, positionMax){
    $(selectorClass).change(function() {
@@ -23,7 +30,8 @@ function populateTeam(selectorClass, positionArray, positionClass, positionCode,
     if(positionArray.indexOf(player) === -1 && positionArray.length < positionMax){
       positionArray.push(player);
       $(positionClass).append(positionCode + positionArray[positionArray.length-1] + cost +'<br />');
-      } else {return false}
+      } else {
+        return false}
     });
 };
 
@@ -35,3 +43,21 @@ populateTeam('.midfielders', midfielderArray, '.mid', 'MID: ', 5)
 populateTeam('.forwards', forwardArray, '.fwd', 'FWD: ', 3)
 
 
+
+// for (var id = 1; id <= 40; id++) {
+//   $.ajax({
+//     url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
+//     dataType: 'json'
+//     }).done(function(data) {
+//         function populateSelectors(Position, selectorClass) {
+//           if(data.type_name === Position) {
+//            $(selectorClass).append(
+//             $('<option></option>').html(data.web_name).val(data.now_cost));
+//         };
+//       }
+//     populateSelectors("Goalkeeper", '.goalkeepers')
+//     populateSelectors("Defender", '.defenders')
+//     populateSelectors("Midfielder", '.midfielders')
+//     populateSelectors("Forward", '.forwards')
+//   });
+// }
